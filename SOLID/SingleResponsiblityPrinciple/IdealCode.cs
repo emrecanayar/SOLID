2797805f13af77b63b8addaa93c7bcfaec87547b
Emre Can Ayar <emrecan.ayar@hotmail.com>
2023-02-13 20:14:54 +0300

    Single Responsibility​ Principle

---
+++ SOLID/SingleResponsiblityPrinciple/IdealCode.cs
@@ -0,0 +1,41 @@
+﻿namespace SingleResponsiblityPrinciple;
+
+/*
+ 
+ Şimdi buradaki sınıf mekanızmalarına bakacak olursak burada NotIdealCode sınıfında yer alan alanları Single Responsiblity Prensibine uygun hale getirmiş olduk.
+
+Database sınıfı içerisinde teknik olarak yer olacak olan Connect , Disconnect metotlarını ve State propertysini tanımladık.
+
+Person için ise bir PersonService sınıfı oluşturduk. Bu sınıf içerisine person ile ilgili verileri getiren GetPersons() adında bir metot oluşturulduk. Bu sayede dabase işlemlerini yapan sınıfı ve person ile ilgili işlemleri yapan sınıfı ayırmış olduk.
+
+Her sınıf kendi içerisindeki yapılardan sorumlu olmuş oldu. Single Responsiblity Prensibinine göre her sınıfın kendine ait bir işi olmalı ve sadece bu iş için geliştirilmeli aynı zamanda sadece bir iş güncellenmelid,r. Bizde bunun neticisinde temelde birbirinden farklı iki ayrı işi iki ayrı sınıfa ayırmış olduk.
+ 
+ */
+
+
+class Database
+{
+    public void Connect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public void Disconnect()
+    {
+        //...
+        Console.WriteLine("Veritabanı bağlantısı kesilmiştir.");
+    }
+    public string State { get; set; }
+}
+
+class PersonService
+{
+    public List<Person> GetPersons()
+    {
+        return new() {
+            new(){ Name = "Hilmi", Surname = "Celayir" },
+            new(){ Name = "Mustafa", Surname = "Yıldız" },
+            new(){ Name = "Cafer", Surname = "Muiddinoğlu" }
+        };
+    }
+}
---
+++ SOLID/SingleResponsiblityPrinciple/NotIdealCode.cs
@@ -0,0 +1,36 @@
+﻿using SingleResponsiblityPrinciple;
+
+namespace SingleResponsiblityPrincipleNotIdealCode;
+
+//Single Responsiblity Prensibine aykırı olarak geliştirilen koda örnek vermek amaçlı oluşturulmuştur.
+class Database
+{
+    /*
+     Bu sınıfın tasarımı Single Responsiblity Prensibine aykırıdır. Çünkü içerisine baktığımızda veri tabanı connect ve disconnect metotlarının yanında başka bir işi yapan GetPersons() metoduna yer almaktadır.
+
+    GetPersons() metodunu incelediğimizde bunun aslında veri tabanı ile direkt olarak alakası yok daha çok veri tabanında var olan bir veriye getirmekle ilgileniyor. İşte bu sebepten dolayı bu metodun bu sınıf içerisinde yer alması uygun değil.
+     
+    Dışarından bakan bir kişi burası için bu sınıf temel veritabanı işlemlerini yapan bir sınıf mı yoksa persons ile ilgili bir sınıf mı diye sorabilir. İşte bunun cevabını biz net olarak veremediğimiz için bu sınıf doğal olarak Single Responsiblity Prensibine aykırıdır.
+
+    Biz doğru yaklaşım için IdealCode classına bir göz atalım.
+     
+     */
+    public void Connect()
+    {
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public void Disconnect()
+    {
+        Console.WriteLine("Veritabanı bağlantısı sağlanmıştır.");
+    }
+    public string State { get; set; }
+
+    public List<Person> GetPersons()
+    {
+        return new() {
+        new(){ Name = "Hilmi", Surname = "Celayir" },
+        new(){ Name = "Mustafa", Surname = "Yıldız" },
+        new(){ Name = "Cafer", Surname = "Muiddinoğlu" }
+    };
+    }
+}
